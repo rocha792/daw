10dbb94447fa8bc0b4ba1b09e12ca0b70d9e15b8
edgarocha <edgarocha792@gmail.com>
2022-03-22 17:58:08 -0700

    fin 1.5

--- NftBid/app/Http/Controllers/Front/IndexController.php
+++ NftBid/app/Http/Controllers/Front/IndexController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
    public function index(){
-       return view('front.index');
+       $categorias = \DB::table('categories')->orderBy('category','ASC') ->get();
+       $productos = \DB::table('nfts')->orderBy('id','DESC')->get();
+       return view('front.index')->with('categories',$categorias)->with('nfts',$productos);
    }
 }
